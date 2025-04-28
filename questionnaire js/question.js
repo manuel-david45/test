3a480ba5bd9746e55031d720e3df9aef15f5987d
manuel-david45 <kouadidavid0@gmail.com>
2025-04-28 16:29:55 +0000

    Add files via upload

---
+++ questionnaire js/question.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    questions: "What does HTML stand for ?",
+    answer: "C. Hyper Text Markup Language",
+    Options: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Home Text Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    questions: "What does CSS stand for ?",
+    answer: "A. Cascading Style Sheet",
+    Options: [
+      "A. Cascading Style Sheet",
+      "B.Cute Style Sheet",
+      "C.Computer Style Sheet",
+      "D.Codehal Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    questions: "What does PHP stand for ?",
+    answer: "A.Hypertext Preprocessor",
+    Options: [
+      "A.Hypertext Preprocessor",
+      "B.Hometext Pregramming",
+      "C.Hypertext Preprogramming",
+      "D.Programming Hypertext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    questions: "What does SQL stand for ?",
+    answer: "D.Structured Query Language",
+    Options: [
+      "A.Strength Query",
+      "B.Stylesheet Query Language",
+      "C.Science Question Language",
+      "D.Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    questions: "What does XML stand for ?",
+    answer: "D. Extensible Markup Language ",
+    Options: [
+      "A.Excellent Multiple Language",
+      "B.Explore Multiple Language",
+      "C.Extra Markup",
+      "D. Extensible Markup Language ",
+    ],
+  },
+];
